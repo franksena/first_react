cfd93e4e5da01631514686cb01e81bbb2e275685
Frank Sena AGBOLOSU <francksena@yahoo.com>
2024-10-24 16:56:37 +0200

    La mise en place de la quête 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
 
+
+
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
   return (
-    <div><PokemonCard/></div>
+    <div><PokemonCard pokemon={pokemonList[0] } /></div>
       
   );
 }
