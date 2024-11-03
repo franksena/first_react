a9685a75097d9a1bce4f2c577f4f1b904e7a5a51
Frank Sena AGBOLOSU <francksena@yahoo.com>
2024-11-03 15:28:35 +0100

    components2 done

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,17 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 
-
-function PokemonCard ({pokemon}){
+function PokemonCard (){
+  const pokemon = pokemonList [0];
 return(
     <figure>
       { pokemon.imgSrc ?
